e61ef9feca8bab9f2caadff6a33315ad7897f9cf
PMSRINIVAS <killermaddy55@gmail.com>
2021-07-07 02:14:12 +0530

    signin and sign out operations completed

--- src/pages/AppNav.js
+++ src/pages/AppNav.js
@@ -1,8 +1,22 @@
-import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { Link, useHistory } from "react-router-dom";
+import { signOutAction } from "../redux/store";
 
 export const AppNav = () => {
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  const signOut = () => {
+    //LOGICAL OPERATION.
+    //COOKIES ANS SESSIONS ARE GETTING REMOVED FROM THE BROWSER
+    dispatch(signOutAction());
+
+    //REDIRECT THE USER TO LOGIN PAGE
+    history.push("/");
+  };
+
   return (
-    <div className="bg-dark text-light p-3 d-flex justify-content-end ">
+    <div className="bg-dark text-light p-3 d-flex justify-content-end align-items-center">
       <Link to="/employee-list">
         <h6 className="mr-3">EMP-List</h6>
       </Link>
@@ -10,6 +24,18 @@ export const AppNav = () => {
       <Link to="/employee-upsert">
         <h6>EMP-Upsert</h6>
       </Link>
+
+      <Link>
+        <h6 className="ml-2 text-danger" onClick={signOut} role="button">
+          Sign Out
+        </h6>
+      </Link>
+      {/* <input
+        type="button"
+        value="Sign Out"
+        onClick={signOut}
+        className="btn btn-info ml-2"
+      /> */}
     </div>
   );
 };

1014953f64a0703ce634b1fc3086046dd5cb2186
PMSRINIVAS <killermaddy55@gmail.com>
2021-07-09 01:43:25 +0530

    customizing according to our project

--- src/pages/AppNav.js
+++ src/pages/AppNav.js
@@ -18,15 +18,15 @@ export const AppNav = () => {
 
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
-      <Navbar.Brand href="#home">EMS App</Navbar.Brand>
+      <Navbar.Brand href="#home">SOCIAL APP</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto">
-          <Nav.Link as={Link} to="/employee-list">
-            EMP-LIST
+          <Nav.Link as={Link} to="/social-book">
+            Social Book
           </Nav.Link>
-          <Nav.Link as={Link} to="/employee-upsert">
-            EMP-UPSERT
+          <Nav.Link as={Link} to="/social-profile">
+            Social Profile
           </Nav.Link>
           <Nav.Link onClick={signOut}>SIGN OUT</Nav.Link>
         </Nav>
